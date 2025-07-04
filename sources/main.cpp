ab9b03634fbaa8294a85f61477db327a03d050e2
krissiliv <pia.gober@gmx.at>
2025-07-04 17:52:38 +0200

    trying to create and bind socket for the first time

--- sources/main.cpp
+++ sources/main.cpp
@@ -16,11 +16,30 @@
 # define GRAY "\x1b[90m"
 
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
 
+// ./ircserv <port> <password>
 int	main(int argc, char *argv[])
 {
 	if (argc != 3)
 		return (std::cerr << RED << "Error: incorrect number of arguments" << DEFAULT << std::endl, 1);
+	
+	int port = std::atoi(argv[1]);
+	if (port <= 0 || port > 65535)
+		return (std::cerr << RED << "Error: invalid port number" << DEFAULT << std::endl, 1);
+	
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);  // Create a TCP/IPv4 socket
+    if (sockfd < 0) 
+    {
+        std::cerr << "Failed to create socket" << std::endl;
+        return 1;
+    }
+
 	(void)argv;
 	return (0);
 }
