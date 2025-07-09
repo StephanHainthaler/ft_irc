944c5febcb769888dca87411e48862d12f976f9f
Jayu <jayuze@gmail.com>
2025-07-09 17:32:16 +0200

    added some variables and functions to client.hpp

--- sources/Client.hpp
+++ sources/Client.hpp
@@ -6,25 +6,31 @@
 /*   By: juitz <juitz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 13:53:54 by juitz             #+#    #+#             */
-/*   Updated: 2025/07/09 15:27:11 by juitz            ###   ########.fr       */
+/*   Updated: 2025/07/09 17:31:18 by juitz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+#include <cstddef>
 #include <iostream>
 #include <string>
 
 class Client
 {
 	private:
-		int			_fd;
+		size_t		_port;
+		size_t		_clientSocketFd;
 		std::string	_IP;
+		std::string _userName;
 		std::string _nickName;
 		std::string _realName;
 	
 	public:
 		int	isNickValid(const std::string& nickName);
-		
+
+		//USER_function
+		//NICK_function
+		//JOIN_function
 		
 		Client();
 		~Client();
