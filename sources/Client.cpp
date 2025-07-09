5978786ff238c57db37ed15717cf038fee432486
Jayu <jayuze@gmail.com>
2025-07-09 16:38:27 +0200

    barebones client

--- sources/Client.cpp
+++ sources/Client.cpp
@@ -6,20 +6,31 @@
 /*   By: juitz <juitz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 13:54:03 by juitz             #+#    #+#             */
-/*   Updated: 2025/07/09 14:32:38 by juitz            ###   ########.fr       */
+/*   Updated: 2025/07/09 16:30:49 by juitz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Client.hpp"
 
+const char* Client::NickNameTooLong::what() const throw()
+{
+   return ("Error: Nickname can only");
+}
+
+const char* Client::NickNameInvalid::what() const throw()
+{
+   return ("Error: file is empty ");
+}
+
 int Client::isNickValid(const std::string& nickName)
 {
 	for (size_t i = 0; i < nickName.size(); i++)
 	{
-		if (nickName[i] == ' ' || nickName[i] == ',' || nickName[i] == '*' || nickName[i] == '?' || nickName[i] == '!' || nickName[i] == '@')
-			return (1);
 		if (nickName.size() > 9)
 			return (1);
+		if (nickName[i] == ' ' || nickName[i] == ',' || nickName[i] == '*' || nickName[i] == '?' || nickName[i] == '!' || nickName[i] == '@')
+			return (1);
+	
 	}
 }
 
--- sources/Client.hpp
+++ sources/Client.hpp
@@ -6,7 +6,7 @@
 /*   By: juitz <juitz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 13:53:54 by juitz             #+#    #+#             */
-/*   Updated: 2025/07/09 14:25:36 by juitz            ###   ########.fr       */
+/*   Updated: 2025/07/09 15:27:11 by juitz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,28 @@
 class Client
 {
 	private:
+		int			_fd;
+		std::string	_IP;
 		std::string _nickName;
 		std::string _realName;
 	
 	public:
 		int	isNickValid(const std::string& nickName);
 		
+		
 		Client();
 		~Client();
 		Client(const Client &other);
-		Client& operator=(const Client &copy); 
+		Client& operator=(const Client &copy);
+
+		class NickNameTooLong : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+		class NickNameInvalid: public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
 };
