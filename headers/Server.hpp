5f858dfacc02afea796726d4465f0f48b5d68827
krissiliv <pia.gober@gmx.at>
2025-07-13 15:42:09 +0200

    pushing first server implementation

--- headers/Server.hpp
+++ headers/Server.hpp
@@ -17,33 +17,98 @@
 #include <sys/socket.h> // for socket, bind, listen, accept
 #include <netinet/in.h> // for sockaddr_in
 
-class Server
+#include <exception>
+#include <vector>
+
+#include "Client.hpp"
+#include "Channel.hpp"
+
+class Server // useful: https:://www.geeksforgeeks.org/cpp/socket-programming-in-cpp/
 {
 	public:
 		Server(const unsigned int &port, const std::string &password);
 		~Server(void);
 		
-		void bindServerSocket(void);
-		void listenServerSocket(void);
-		void acceptClientConnections(void);
+		// Getters
+		std::string get_password(void) const;
+		Channel *get_channel(const std::string &channel_name) const;
 		
+
+		// Member functions - server actions
+		void makeListen(void);
+		void acceptClientConnection(Client *client);
 		void handleClientMessage(int client_fd);
+		
+
+		// Member functions - user triggered actions
+		void addClient(Client *client);
+		void removeClient(Client *client);
+		
+		void addChannel(Channel *channel);
+		void removeChannel(Channel *channel);
+		
+
+		class ServerException: public std::exception
+		{
+			public:
+				ServerException(std::string message) : _message(message) {}
+				const char* what() const throw();
+			
+			private :
+				std::string _message;
+		};
 
 	private:
 		Server(void);
 		Server(const Server &other);
 		Server	&operator=(const Server &other);
+	
+		
+		int						_socket_fd; // Server Socket FD - Listening socket, can be negative
+		const unsigned int 		_port; // Port number - "door to the server"
+		struct sockaddr_in 		_serverAddress; // for IPv4 - holds network info - like IP address and port number - that the server uses to know where to listen or connect
+		// struct sockaddr_in6 	_serverAddress; // for IPv6 - holds network info - like IP address and port number - that the server uses to know where to listen or connect
+		/*
+		server creates sockaddr_in "serverAddress" to specify its own IP address and port to bind to
+		client takes this sockaddr_in "serverAddress" to specify the server's IP address and port to connect to
+		*/
 
-		
-		const unsigned int	socket_fd; // Server Socket FD - Listening socket (IP + port combo)
-		// IP
-		const unsigned int port;		
-		struct sockaddr_in sin; // holds network info - like IP address and port number - that the server uses to know where to listen or connect
-		
-		const std::string	password;
-			// List of connected clients (ClientClass objs)
-			// List of channels (ChannelClass objs)
+		const std::string		_password;
+		std::vector<Client *>	_clients;	// List of connected clients (ClientClass objs)
+		std::vector<Channel *>	_channels;	// List of channels (ChannelClass objs)
 
 };
 
 #endif
+
+/* 
+***USED FUNCTIONS:
+socket(AF_INET, SOCK_STREAM, 0)close(fd)
+connect(sockfd, (sockaddr*)&addr, sizeof(addr))
+bind(sockfd, (sockaddr*)&addr, sizeof(addr))
+listen(sockfd, SOMAXCONN)
+accept(sockfd, NULL, NULL)
+htons()
+fcntl(sockfd, F_SETFL, O_NONBLOCK)
+
+***MIGHT BE USEFUL:
+setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes))
+recv(client_fd, buffer, sizeof(buffer), 0)
+lseek(fd, 0, SEEK_END)
+
+***UNUSED FUNCTIONS:
+getsockname(sockfd, (sockaddr*)&addr, &len)getprotobyname("tcp")
+gethostbyname("irc.example.com")
+getaddrinfo("localhost", "6667", &hints, &res)
+freeaddrinfo()
+ntohs()
+htonl()
+ntohl(addr.sin_addr.s_addr)
+inet_addr("127.0.0.1")
+inet_ntoa(addr.sin_addr)
+send(client_fd, buffer, len, 0)
+signal(SIGINT, handle_sigint)
+sigaction(SIGINT, &sa, NULL)
+fstat(fd, &info)
+poll(fds, nfds, timeout)
+*/
--- sources/Server.cpp
+++ sources/Server.cpp
@@ -12,40 +12,203 @@
 
 #include "../headers/Server.hpp"
 
+Server::Server(void) // private
+{
+}
+
+/* sockaddr_in / sockaddr_in6 
+is a structure specifically for handling IPv4 addresses
+In subj it says "communication between client and server has to be done via TCP/IP (v4 or v6)"
+
+the IP address is specified by the computer I am running the server code on,
+and the port number is specified by the user (as a command line argument)
+*/
 Server::Server(const unsigned int &port, const std::string &password): port(port), password(password)
 {
-    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);  // Create a TCP/IPv4 socket
-    if (socket_fd == -1) 
-    {
-        std::cerr << "Error. Failed to create server socket." << std::endl;
-        return 1;
-    }
+	// AF_INET specifies IPv4 protocol family, SOCK_STREAM specifies TCP protocol
+    _socket_fd = socket(AF_INET, SOCK_STREAM, 0); // Create a TCP/IPv4 socket
+    if (_socket_fd == -1) 
+	{
+		throw ServerException("Error. Failed to create socket.");
+	}
+	if (fcntl(socket_fd, F_SETFL, O_NONBLOCK) == -1) // allow server to handle multiple clients at once
+	{
+		close(_socket_fd);
+		throw ServerException("Error. Failed to set socket to non-blocking mode.");
+	}
     
-    memset(&sin, 0, sizeof(sin)); // recommended to not store nonsense
-    sin.sin_family = AF_INET; // set the address family to IPv4 addresse
-    sin.sin_addr.s_addr = INADDR_ANY; // server should accept connections from any IP address
-    sin.sin_port = htons(port); // defines the port number the socket will use to communicate on server side (the value has to be converted from host byte order to network byte order)
+	_serverAddress.sin_family = AF_INET; // set the address family to IPv4 addresses
+	_serverAddress.sin_addr.s_addr = INADDR_ANY; // server should accept connections from any IPv4 address, used when we don't want to bind our socket to any particular IP, to mak eit listen to all available IPs
+	_serverAddress.sin_port = htons(port); // defines the port number the socket will use to communicate on server side (the value has to be in network byte order)
+
+	// _clients and _channels remain empty at this point (?) - will get filled later
+
+	if (bind(_socket_fd, (struct sockaddr_in6 *)&_serverAddress, sizeof(_serverAddress)) == -1)
+	{
+		close(_socket_fd);
+		throw ServerException("Error. Failed to bind socket.");
+	}
+	std::cout << GRAY << "Server socket bound to port " << _port << DEFAULT << std::endl;
+}
+
+Server::Server(const Server &other) // private
+{
+	_socket_fd = other._socket_fd;
+	_port = other._port;
+	_serverAddress = other. _serverAddress;
+	_password = other._password;
+	_clients = other._clients;
+	_channels = other._channels;
+}
+
+Server	&Server::operator=(const Server &other) // private
+{
+	if (this != &other)
+	{
+		_socket_fd = other._socket_fd;
+		_port = other._port;
+		_serverAddress = other. _serverAddress;
+		_password = other._password;
+		_clients = other._clients;
+		_channels = other._channels;
+	}
+	return *this;
+}
+
+Server::~Server(void)
+{
+	if (socket_fd != -1)
+		close(socket_fd);
+
+	// clear client vector (BUGFIX? - needed?)
+	if (!_clients.empty())
+	{
+		for (std::vector<Client *>::iterator it = _clients.begin(); it != _clients.end(); ++it)
+		{
+			delete *it;
+		}
+		_clients.clear();
+	}
+
+	// clear channel vector (BUGFIX? - needed?)
+	if (!_channels.empty())
+	{
+		for (std::vector<Channel *>::iterator it = _channels.begin(); it != _channels.end(); ++it)
+		{
+			delete *it;
+		}
+		_channels.clear();
+	}
 
 }
 
+// Getters
+std::string Server::get_password(void) const
+{
+	return _password;
+}
 
-
-	if (bind(sockfd, (struct sockaddr *)&sin, sizeof(sin)) == -1) // Binds a socket to an IP and port and must be done before listening
+Channel *Server::get_channel(const std::string &channel_name) const
+{
+	for (std::vector<Channel *>::const_iterator it = _channels.begin(); it != _channels.end(); ++it)
 	{
-		std::cerr << "Error. Failed to bind socket." << std::endl;
-		close(sockfd);
-		return 1;
+		if ((*it)->get_name() == channel_name)
+			return *it;
 	}
+	return NULL;
+}
 
-	if (listen(sockfd, SOMAXCONN) == -1) // Marks a bound socket as passive, meaning it will be used to accept incoming connection requests
+// Member functions - server actions
+
+// Marks a bound socket as "listening socket"
+// Server is on receiving end of data so needs to listen for incoming connections
+void Server::makeListen(void)
+{
+	if (listen(_socket_fd, SOMAXCONN) == -1)
 	{
-		std::cerr << "Error. Failed to listen on socket." << std::endl;
-		close(sockfd);
-		return 1;
+		close(_socket_fd);
+		throw ServerException("Error. Failed to listen on socket.");
 	}
+}
 
-	std::cout << GRAY << "Server is listening on port " << port << DEFAULT << std::endl;
+void Server::acceptClientConnection(Client *client)
+{
+	if (client == NULL)
+		return;
 
-	...
-	close(sockfd); // Close the server socket when done
-	std::cout << GRAY << "Server socket closed." << DEFAULT << std::endl;
+	int client_fd = accept(_socket_fd, NULL, NULL);
+	if (client_fd == -1) 
+	{
+		std::cerr << "Error. Failed to accept connection." << std::endl;
+		return;
+	}
+	addClient(client);
+}	
+
+void Server::handleClientMessage(int client_fd)
+{
+	return; // think about this
+}
+
+// Member functions - user triggered actions
+void Server::addClient(Client *client)
+{
+	if (client == NULL)
+		return;
+	
+	for (std::vector<Client *>::iterator it = _clients.begin(); it != _clients.end(); ++it)
+	{
+		if (*it == client)
+			return; 
+	}
+	_clients.push_back(client);
+}
+
+void Server::removeClient(Client *client)
+{
+	if (client == NULL)
+		return;
+
+	for (std::vector<Client *>::iterator it = _clients.begin(); it != _clients.end(); ++it)
+	{
+		if (*it == client)
+		{
+			_clients.erase(it);
+			break;
+		}
+	}
+}
+
+void Server::addChannel(Channel *channel)
+{
+	if (channel == NULL)
+		return;
+
+	for (std::vector<Channel *>::iterator it = _channels.begin(); it != _channels.end(); ++it)
+	{
+		if (*it == channel)
+			return; 
+	}
+	_channels.push_back(channel);
+}
+
+void Server::removeChannel(Channel *channel)
+{
+	if (channel == NULL)
+		return;
+
+	for (std::vector<Channel *>::iterator it = _channels.begin(); it != _channels.end(); ++it)
+	{
+		if (*it == channel)
+		{
+			_channels.erase(it);
+			break;
+		}
+	}
+}
+
+// Exception
+const char* Server::ServerException::what() const throw()
+{
+	return _message.c_str();
+}
