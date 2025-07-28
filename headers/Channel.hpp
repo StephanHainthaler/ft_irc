4763a63ade9477dd60a5373d29ad43d62844a8a1
krissiliv <pia.gober@gmx.at>
2025-07-28 11:35:58 +0200

    adding channel classs

--- headers/Channel.hpp
+++ headers/Channel.hpp
@@ -16,15 +16,36 @@
 # include <iostream>
 # include <vector>
 
+# include <Client.hpp>
+
+/*
+3. ChannelClass
+    user_limit
+    password (private Channels) - optional / can be NULL, _k für key    (private attribute)
+    	KICK-      Eject a client from the channel
+        INVITE-    Invite a client to a channel
+        TOPIC-     Change or view the channel topic
+        MODE-      Change the channels mode:
+            · i: Set/remove Invite-only channel
+            · t: Set/remove the restrictions of the TOPIC command to channel operators
+            · k: Set/remove the channel key (password)
+            · o: Give/take channel operator privilege
+            · l: Set/remove the user limit to channel
+*/
 class Channel
 {
 	public:
 		~Channel(void);
 
 		std::string					getName(void) const;
-		std::vector<std::string>	getChannelUsers(void) const;
 		std::string					getTopic(void) const;
 		void						setTopic(std::string topic);
+		std::string					getState(void) const;
+		void						setState(std::string state);
+		std::string					getMode(void) const;
+		std::vector<Client *>		getChannelUsers(void) const;
+		std::vector<Client *>		getOperators(void) const;
+		Client						*getUser(const std::string &nickname) const;
 
 	private:
 		Channel(void);
@@ -32,10 +53,11 @@ class Channel
 		Channel	&operator=(const Channel &other);
 
 		std::string					_name; //Added By Stephan for compiling
-		std::vector<std::string>	_channelUsers;	//Added By Stephan for compiling
 		std::string					_topic;	//Added By Stephan for compiling
-
-
+		std::string					_state;
+		std::string					_mode;	// (e.g., +i, +t)
+		std::vector<Client *>		_channelUsers;	//Added By Stephan for compiling
+		std::vector<Client *>		_operators; // (ClientClass objs with operator status)
 };
 
 #endif
--- sources/Channel.cpp
+++ sources/Channel.cpp
@@ -17,11 +17,6 @@ std::string Channel::getName(void) const
 	return _name;
 }
 
-std::vector<std::string> Channel::getChannelUsers(void) const
-{
-	return _channelUsers;
-}
-
 std::string	Channel::getTopic(void) const
 {
 	return _topic;
@@ -30,4 +25,39 @@ std::string	Channel::getTopic(void) const
 void	Channel::setTopic(std::string topic)
 {
 	_topic = topic;
+}
+
+std::string Channel::getState(void) const
+{
+	return _state;
+}
+
+void Channel::setState(std::string state)
+{
+	_state = state;
+}
+
+std::string Channel::getMode(void) const
+{
+	return _mode;
+}
+
+std::vector<Client *> Channel::getChannelUsers(void) const
+{
+	return _channelUsers;
+}
+
+std::vector<Client *> Channel::getOperators(void) const
+{
+	return _operators;
+}
+
+Client *Channel::getUser(const std::string &nickname) const
+{
+	for (std::vector<Client *>::const_iterator it = _channelUsers.begin(); it != _channelUsers.end(); ++it)
+	{
+		if ((*it)->getNickname() == nickname)
+			return *it;
+	}
+	return NULL;
 }
